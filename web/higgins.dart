072a891e059e52fb0d0d63ebe6d649097d078b99
VincentBostoen <vincent.bostoen@gmail.com>
2013-01-29 11:46:03 +0100

    Hello Bootstrap

---
+++ web/higgins.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+int startingCount = 5; // initial value for click-counter
+
+void main() {
+  useShadowDom = true; // to enable use of experimental Shadow DOM in the browser  
+}
