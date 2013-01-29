7b43ec40a41e04b025a407a4840bf114d7e201dd
Vincent Bostoen <vincent.bostoen@gmail.com>
2013-01-29 18:55:01 +0100

    Remove generated content

--- web/higgins.dart
+++ web/higgins.dart
@@ -1,8 +1,4 @@
 import 'dart:html';
-import 'package:web_ui/web_ui.dart';
-
-int startingCount = 5; // initial value for click-counter
 
 void main() {
-  useShadowDom = true; // to enable use of experimental Shadow DOM in the browser  
 }
