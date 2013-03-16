d204f953bf2ddf563fdeac1814bde9ec3c555fe4
Guillaume Girou <girou.g@sfeir.com>
2013-03-16 19:56:38 +0100

    Added an input element to send git url

--- web/command.dart
+++ web/command.dart
@@ -1,12 +1,36 @@
 import 'dart:html';
+import 'dart:json' as JSON;
 
+InputElement _gitUrlInput = query('#gitUrl');
+InputElement _buildButton = query('#buildButton');
 Element _textElement = query('#text');
 
 main() {
-  _textElement.text = "ping -c 10 google.fr";
-  var source = new EventSource('/command/');
+  _gitUrlInput.value = "https://github.com/ggirou/higgins-server.git";
+  _buildButton.onClick.listen((_) => build());
+}
+
+build() {
+  var gitUrl = _gitUrlInput.value;
+  _textElement.text = "Build $gitUrl";
+
+  String data = JSON.stringify({"git_url": gitUrl});
+  HttpRequest.request("/build/", method: "POST", sendData: data).then((request) {
+    String responseData = request.responseText;
+    print("responseData: $responseData");
+    
+    var response = JSON.parse(responseData);
+    showCommandResult(response["build_id"]);
+  });
+}
+
+void showCommandResult(String buildId) {
+  var source = new EventSource('/command/$buildId/');
   source
-    ..onOpen.listen((_) => print("Open"))
-    ..onMessage.listen((MessageEvent me)  => _textElement.innerHtml = "${_textElement.innerHtml}<br>${me.data}")
-    ..onError.listen((e) => print("Error $e"));
+  ..onOpen.listen((_) => print("Open EventSource"))
+  ..onMessage.listen((MessageEvent me) {
+    print(me.data);
+    _textElement.innerHtml = "${_textElement.innerHtml}<br>${me.data}";
+  })
+  ..onError.listen((e) => print("Error $e"));
 }
